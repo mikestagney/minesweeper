863f807799f24260989f750786c86a86db72c0a9
mikestagney <mikestagney@yahoo.com>
2021-07-23 17:53:59 -0400

    passed stage 2 tests

--- Main.java
+++ Main.java
@@ -1,21 +1,42 @@
 package minesweeper;
 
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        char[][] board = new char[9][9];
+        Scanner input = new Scanner(System.in);
         Random random = new Random();
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = random.nextInt(10) == 9 ? 'X' : '.';
-                System.out.print(board[i][j]);
+        final int SIZE = 9;
+        final char MINE = 'X';
+        final char SAFE = '.';
+
+
+        char[][] board = new char[SIZE][SIZE];
+        System.out.println("How many mines do you want on the field?");
+        int mines = Integer.parseInt(input.nextLine());
+
+        for (char[] row : board) {
+            Arrays.fill(row, SAFE );
+        }
+
+        while (mines > 0) {
+            int row = random.nextInt(SIZE);
+            int col = random.nextInt(SIZE);
+            if (board[row][col] == SAFE) {
+                board[row][col] = MINE;
+                mines--;
+            }
+        }
+
+        for (char[] row : board) {
+            for (char columnSquare : row) {
+                System.out.print(columnSquare);
             }
             System.out.println();
         }
-
-
         
     }
 }
